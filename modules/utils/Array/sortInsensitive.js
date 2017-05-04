31cad2c40c92008e892ceb1281eef2bd31a32b21
tmarshall <timothyjmarshall@gmail.com>
2017-05-03 20:27:02 -0700

    array sort insenitive util

---
+++ modules/utils/Array/sortInsensitive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function sortInsensitive(arr, key) {
+  // see http://stackoverflow.com/questions/8996963/how-to-perform-case-insensitive-sorting-in-javascript
+  arr.sort((a, b) => {
+    const aVal = key === undefined ? a : a[key];
+    const bVal = key === undefined ? b : b[key];
+
+    return aVal.toLowerCase().localeCompare(bVal.toLowerCase());
+  });
+};
