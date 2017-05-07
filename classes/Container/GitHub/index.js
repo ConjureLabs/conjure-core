471106e89b23460904ae8f6d1a495d68448f462b
tmarshall <timothyjmarshall@gmail.com>
2017-05-07 13:06:59 -0700

    create container, with github, updated to show more (comment) context

--- classes/Container/GitHub/index.js
+++ classes/Container/GitHub/index.js
@@ -2,35 +2,56 @@
 
 const Container = require('../');
 const log = require('../../../modules/log')('github container');
+const async = require('async');
+const config = require('../../../modules/config');
+
+const {
+  protocol,
+  domain
+} = config.app;
+
+const gitHubCommentSignature = [
+  '',
+  '---',
+  '',
+  `<kbd>⎔</kbd> __This message was create via [Conjure.sh](${protocol}://${domain})__`
+];
 
 class GitHubContainer extends Container {
   // saving github comment, when creating a new container
   create(callback) {
-    super.create((err, hostPort) => {
-      if (err) {
-        return callback(err);
-      }
+    const waterfall = [];
 
+    // commenting on issue thread to notify that an instance is spinning up
+    waterfall.push(cb => {
+      issue.upsertComment([
+        `:hourglass_flowing_sand: [Conjure.sh](${protocol}://${domain}) is spinning up this branch`,
+      ].concat(gitHubCommentSignature).join('\n'), err => {
+        cb(err);
+      });
+    });
+
+    // create vm
+    waterfall.push(cb => {
+      super.create((err, hostPort) => {
+        cb(err, hostPort);
+      });
+    });
+
+    waterfall.push((hostPort, cb) => {
       const Issue = require('../../Repo/GitHub/Issue');
       const issue = new Issue(this.payload);
 
       const config = require('../../../modules/config');
-      const {
-        protocol,
-        domain
-      } = config.app;
-      const { branch } = this.payload;
 
       issue.upsertComment([
         `:octocat: [You can view this branch at ${protocol}://${domain}:${hostPort}](${protocol}://${domain}:${hostPort})`,
-        '',
-        '---',
-        '',
-        `<kbd>⎔</kbd> __This message was create via [Conjure.sh](${protocol}://${domain})__`
-      ].join('\n'), err => {
-        callback(err);
+      ].concat(gitHubCommentSignature).join('\n'), err => {
+        cb(err);
       });
     });
+
+    async.waterfall(waterfall, callback);
   }
 
   destroy(callback) {
