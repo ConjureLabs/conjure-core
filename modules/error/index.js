24edddf6dd36b5f6107682dda810b5ba5b3dcd25
tmarshall <timothyjmarshall@gmail.com>
2017-05-23 19:42:22 -0700

    adding custom Error constructors

---
+++ modules/error/index.js
@@ -0,0 +1,22 @@
+const nativeErrorKeys = ['address', 'code', 'errno', 'name', 'path', 'port', 'stack', 'syscall']; // omitting 'message' since that will be applied automatically
+
+class ConjureError extends Error {
+  static from(err) {
+    const Constructor = this;
+    const newError = new Constructor(err.message || err.toString());
+
+    for (let i = 0; i < nativeErrorKeys.length; i++) {
+      if (err[ nativeErrorKeys[i] ] === undefined) {
+        continue;
+      }
+
+      newError[ nativeErrorKeys[i] ] = err[ nativeErrorKeys[i] ];
+    }
+
+    return newError;
+  }
+}
+module.exports.ConjureError = ConjureError;
+
+class NotFoundError extends ConjureError {}
+module.exports.NotFoundError = NotFoundError;
