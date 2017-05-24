ff05d3056c5785ed975b8c8e3fa48eb1e5bfc385
tmarshall <timothyjmarshall@gmail.com>
2017-05-23 19:58:38 -0700

    comments for clarity

--- modules/err/index.js
+++ modules/err/index.js
@@ -1,5 +1,6 @@
 const nativeErrorKeys = ['address', 'code', 'errno', 'name', 'path', 'port', 'stack', 'syscall']; // omitting 'message' since that will be applied automatically
 
+// basic error on our side
 class ConjureError extends Error {
   static from(err) {
     const Constructor = this;
@@ -18,14 +19,18 @@ class ConjureError extends Error {
 }
 module.exports.ConjureError = ConjureError;
 
+// if something was not found (on web, should trigger a 404)
 class NotFoundError extends ConjureError {}
 module.exports.NotFoundError = NotFoundError;
 
+// permissions are not valid
 class PermissionsError extends ConjureError {}
 module.exports.PermissionsError = PermissionsError;
 
+// an error occurred where we don't think it should ever
 class UnexpectedError extends ConjureError {}
 module.exports.UnexpectedError = UnexpectedError;
 
+// missing data, wrong keys passed, etc
 class ContentError extends ConjureError {}
 module.exports.ContentError = ContentError;
