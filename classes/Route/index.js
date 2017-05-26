4b0e9e88c0f2734ea498fbf41db7426fe0825fee
tmarshall <timothyjmarshall@gmail.com>
2017-05-26 11:34:17 -0400

    set up CORS

--- classes/Route/index.js
+++ classes/Route/index.js
@@ -1,7 +1,19 @@
+const cors = require('cors');
+const config = require('conjure-core/modules/config');
 const log = require('conjure-core/modules/log')('Route');
 
 const requireAuthenticationWrapper = Symbol('Require Auth Wrapper');
 const vanillaWrapper = Symbol('Vanilla (non-additive) Wrapper');
+
+const corsOptions = {
+  methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'HEAD', 'OPTIONS'],
+  optionsSuccessStatus: 200,
+  origin: [
+    config.app.api.url,
+    config.app.web.url
+  ],
+  preflightContinue: true
+}
 
 class Route extends Array {
   constructor(options) {
@@ -54,7 +66,9 @@ class Route extends Array {
     for (let i = 0; i < this.length; i++) {
       log.info(verb.toUpperCase(), expressPathUsed);
 
-      router[expressVerb](expressPathUsed, (
+      // see https://github.com/expressjs/cors#enabling-cors-pre-flight
+      routes.options(expressPathUsed, cors(corsOptions));
+      router[expressVerb](expressPathUsed, cors(corsOptions), (
         this.requireAuthentication ? this[requireAuthenticationWrapper](this[i]) : this[i]
       ));
     }
