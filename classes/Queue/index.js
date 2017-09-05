3fc5bb85199fe566fe5385def5732357d04c4693
tmarshall <timothyjmarshall@gmail.com>
2017-09-04 17:07:23 -0700

    messages and ack working

--- classes/Queue/index.js
+++ classes/Queue/index.js
@@ -13,8 +13,6 @@ function getConnection(callback) {
   const log = require('conjure-core/modules/log')('MQ');
   const config = require('conjure-core/modules/config').mq;
 
-  console.log(config);
-
   const connection = amqp.createConnection(config);
 
   connection.on('error', err => {
@@ -28,25 +26,31 @@ function getConnection(callback) {
 
     // process all pending callbacks
     for (let i = 0; i < pendingConnectionCallbacks.length; i++) {
-      pendingConnectionCallbacks[i](null, connection);
+      const pending = pendingConnectionCallbacks.shift();
+      pending(null, connection);
     }
   });
 }
 
-const pendingQueueReady = Symbol('pending queue ready');
 const onQueueReady = Symbol('on queue ready event');
+const exchangeName = Symbol('exchange name');
+const queueName = Symbol('queue name');
+const routingKey = Symbol('routing key for queue messages');
 class Queue {
-  constructor(exchangeName, queueName) {
-    this[pendingQueueReady] = [];
+  constructor(exchangeNameArg, queueNameArg, routingKeyArg) {
+    this[exchangeName] = exchangeNameArg;
+    this[queueName] = queueNameArg;
+    this[routingKey] = routingKeyArg;
+  }
 
+  [onQueueReady](callback) {
     getConnection((err, connection) => {
       if (err) {
-        // todo: handle errs?
-        throw err;
+        return callback(err);
       }
 
       // see https://github.com/postwait/node-amqp#connectionexchangename-options-opencallback
-      connection.exchange(exchangeName, {
+      connection.exchange(this[exchangeName], {
         type: 'topic',
         passive: false,
         durable: true, // keep true to persist exchange
@@ -55,38 +59,30 @@ class Queue {
         confirm: true // causes exchange.publish to fire callback w/ failure bool
       }, exchange => {
         // see https://github.com/postwait/node-amqp#connectionqueuename-options-opencallback
-        connection.queue(queueName, {
+        connection.queue(this[queueName], {
           passive: false,
           durable: true, // keep true to persist queue
           exclusive: false,
           autoDelete: false,
           noDeclare: false
         }, queue => {
-          this[onQueueReady] = callback => callback(null, exchange, queue);
-
-          for (let i = 0; i < this[pendingQueueReady].length; i++) {
-            this[pendingQueueReady][i](null, exchange, queue);
-          }
-
-          this[pendingQueueReady] = null;
+          queue.bind(this[exchangeName], this[routingKey]);
+          callback(null, exchange, queue);
         });
       });
     });
+
+    return this;
   }
 
-  // will be replaced with a direct callback call when queue is ready
-  [onQueueReady](callback) {
-    this[pendingQueueReady].push(callback);
-  }
-
-  publish(routingKey, message, callback = function() {}) {
-    this[onQueueReady]((err, exchange) => {
+  publish(message, callback = function() {}) {
+    return this[onQueueReady]((err, exchange) => {
       if (err) {
         return callback(err);
       }
 
       // see https://github.com/postwait/node-amqp#exchangepublishroutingkey-message-options-callback
-      exchange.publish(routingKey, message, {
+      exchange.publish(this[routingKey], message, {
         mandatory: true,
         deliveryMode: 2, // persistent
         priority: 1, // todo: make priority adjustable?
@@ -104,18 +100,15 @@ class Queue {
   }
 
   subscribe(callback) {
-    this[onQueueReady]((err, _, queue) => {
+    return this[onQueueReady]((err, _, queue) => {
       if (err) {
         return callback(err);
       }
 
       // see https://github.com/postwait/node-amqp#queuesubscribeoptions-listener
       queue.subscribe({
-        ack: true,
-        prefetchCount: 1
-      }, (message, headers, deliveryInfo, ack) => {
-        callback(null, new Message(message, ack));
-      });
+        ack: true
+      }, (message, headers, deliveryInfo, ack) => callback(null, new Message(message, ack)));
     });
   }
 }
@@ -128,6 +121,7 @@ class Message {
 
   ack() {
     this.rawAck.acknowledge();
+    return this;
   }
 }
 
