09cb66d712586ecc8704c020fbfe43618507f322
tmarshall <timothyjmarshall@gmail.com>
2017-10-30 18:46:31 -0700

    -.

--- modules/database/index.js
+++ modules/database/index.js
@@ -9,7 +9,7 @@ async function query(...args) {
   const client = await pool.connect();
 
   log.dev.info(args[0] /* sql */, args[1] || [] /* placeholder values */);
-  const result = await.client.query(...args);
+  const result = await client.query(...args);
 
   client.release();
 
