cae1121a8d2c4f1fadb712da51bb1220a10a2c4e
tmarshall <timothyjmarshall@gmail.com>
2017-11-04 14:57:42 -0700

    updated lint setup

--- bash/yarn/lint.sh
+++ bash/yarn/lint.sh
@@ -3,19 +3,10 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 . $BASE/../functions.cfg;
 
-if ! hash jscs 2>/dev/null; then
-  error "Could not run jscs - you may need to run 'npm install jscs -g'";
-  exit 1;
-fi
-
-if ! hash eslint 2>/dev/null; then
-  error "Could not run eslint - you may need to run 'npm install eslint -g'";
-  exit 1;
-fi
-
 set -e;
 
 eslint ./**/*.js --quiet;
-jscs ./**/*.js;
-
 progress "Lint passed";
+
+jscs ./**/*.js;
+progress "Coding style passed";
