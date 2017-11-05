ec687de7734a3a8e4fcc4654d0db3a3fad068fde
tmarshall <timothyjmarshall@gmail.com>
2017-11-04 23:27:09 -0700

    resetting child process to non async, as that is expected in worker

--- modules/childProcess/exec.js
+++ modules/childProcess/exec.js
@@ -1,28 +1,26 @@
-const { ConjureError } = require('../err');
+const ConjureError = require('conjure-core/modules/err').ConjureError;
 
-const log = require('../log')('child process execution');
+const log = require('conjure-core/modules/log')('child process execution');
 
-module.exports = (command, options) => {
-  return new Promise((resolve, reject) => {
-    const exec = require('child_process').exec;
+module.exports = (command, options, callback) => {
+  const exec = require('child_process').exec;
 
-    log.dev.info(command);
+  log.dev.info(command);
 
-    exec(command, options, (err, stdout, stderr) => {
-      if (err) {
-        return reject(err);
-      }
+  exec(command, options, (err, stdout, stderr) => {
+    if (err) {
+      return callback(err);
+    }
 
-      if (stderr) {
-        return reject(new ConjureError(stderr));
-      }
+    if (stderr) {
+      return callback(new ConjureError(stderr));
+    }
 
-      // // keeping these commented lines to help debug, when needed
-      // if (stdout) {
-      //   console.log(stdout);
-      // }
+    // // keeping these commented lines to help debug, when needed
+    // if (stdout) {
+    //   console.log(stdout);
+    // }
 
-      resolve(typeof stdout === 'string' ? stdout.trim() : '');
-    });
+    callback(null, typeof stdout === 'string' ? stdout.trim() : '');
   });
 };
