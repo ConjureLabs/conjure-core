8a2253de01f8b3faf79b18cc56bf289048a71e34
tmarshall <timothyjmarshall@gmail.com>
2018-01-31 16:05:12 -0800

    removed utils, since they are now in a new (public) repo

--- modules/utils/Array/sort-insensitive.js
+++
@@ -1,9 +0,0 @@
-module.exports = function sortInsensitive(arr, key) {
-  // see http://stackoverflow.com/questions/8996963/how-to-perform-case-insensitive-sorting-in-javascript
-  arr.sort((a, b) => {
-    const aVal = key === undefined ? a : a[key];
-    const bVal = key === undefined ? b : b[key];
-
-    return aVal.toString().toLowerCase().localeCompare(bVal.toString().toLowerCase());
-  });
-};
