29d45f01357b7d84eb146fbfa202359f80f28470
tmarshall <timothyjmarshall@gmail.com>
2018-02-18 14:48:41 -0800

    +queue.removeAll()

--- classes/Queue/index.js
+++ classes/Queue/index.js
@@ -46,6 +46,60 @@ class Queue {
   subscribe(handler, parallelCount = 1) {
     this.queue.process(this.type, parallelCount, handler);
   }
+
+  async removeAll() {
+    // first clear all pending, ensure they don't bump into active
+    await cacelAllJobsByState(this.type, 'inactive');
+    // now clear active jobs
+    await cacelAllJobsByState(this.type, 'active');
+  }
+}
+
+function cacelAllJobsByState(type, state) {
+  return new Promise(async (resolve, reject) => {
+    const batchSize = 20;
+    let canceledCount = 0;
+
+    do {
+      let batchResults;
+
+      try {
+        batchResults = await cancelJobsByStateBatch(type, state, batchSize);
+      } catch(err) {
+        return reject(err);
+      }
+
+      canceledCount = batchResults.length;
+    } while (canceledCount === batchSize);
+
+    resolve();
+  });
+}
+
+function cancelJobsByStateBatch(type, state, batchSize) {
+  return new Promise((resolve, reject) => {
+    kue.Job.rangeByType(type, state, 0, batchSize, 'asc', async (err, jobs) => {
+      if (err) {
+        return reject(err);
+      }
+
+      Promise
+        .all(jobs.map(job => cancelIndividualJob(job)))
+        .then(resolve)
+        .catch(reject);
+    });
+  });
+}
+
+function cancelIndividualJob(job) {
+  return new Promise((resolve, reject) => {
+    job.remove(err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
 }
 
 module.exports = Queue;
