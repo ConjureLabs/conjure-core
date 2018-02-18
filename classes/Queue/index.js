aa9bd52e0db11b7e9f6c3b92f8e004a00a4358a3
tmarshall <timothyjmarshall@gmail.com>
2018-02-18 15:41:00 -0800

    remove kue, use Bee-Queue

--- classes/Queue/index.js
+++ classes/Queue/index.js
@@ -1,35 +1,30 @@
-const kue = require('kue');
+const BeeQueue = require('bee-queue');
 const config = require('../../modules/config');
 const log = require('../../modules/log')('Queue');
 
 class Queue {
-  constructor(type) {
-    this.queue = kue.createQueue({
-      prefix: config.redis.queue.prefix,
-      redis: {
-        port: config.redis.port,
-        host: config.redis.host,
-        auth: config.redis.auth
-      },
-      jobEvents: false
+  constructor(type, isWorker = false) {
+    this.queue = new BeeQueue(type, {
+      redis: config.redis,
+      isWorker
     });
     this.type = type;
   }
 
   // to queue a job
-  push(attributes = {}, priority = 'low') {
+  // `grouping` is a required string, that should be unique to the branch & PR combination
+  // it is used to track pending jobs, so we can clear them as needed
+  push(grouping, attributes = {}, priority = 'normal') {
     return new Promise(resolve, reject) => {
       const unitsOfTime = require('../../modules/unitsOfTime');
 
       this.queue
-        .create(this.type, attributes)
-        .priority(priority) // see https://github.com/Automattic/kue#job-priority
-        .attempts(3)
-        .backoff({
-          type: 'exponential'
-        })
-        .ttl(unitsOfTime.hour)
-        .removeOnComplete(true)
+        .createJob(attributes)
+        .retries(3)
+        .backoff('exponential', unitsOfTime.second * 2)
+        .timeout(unitsOfTime.hour)
+        .removeOnSuccess(true)
+        .removeOnFailure(true)
         .save(err => {
           if (err) {
             return reject(err);
@@ -40,66 +35,16 @@ class Queue {
       this.queue.on('errror', err => {
         log.error(err);
       });
+
+      this.queue.on('retrying', (job, err) => {
+        log.info(`job ${job.id} is retrying - err message of "${err.message}"`);
+      });
     });
   }
 
   subscribe(handler, parallelCount = 1) {
-    this.queue.process(this.type, parallelCount, handler);
+    this.queue.process(parallelCount, handler);
   }
-
-  async removeAll() {
-    // first clear all pending, ensure they don't bump into active
-    await cacelAllJobsByState(this.type, 'inactive');
-    // now clear active jobs
-    await cacelAllJobsByState(this.type, 'active');
-  }
-}
-
-function cacelAllJobsByState(type, state) {
-  return new Promise(async (resolve, reject) => {
-    const batchSize = 20;
-    let canceledCount = 0;
-
-    do {
-      let batchResults;
-
-      try {
-        batchResults = await cancelJobsByStateBatch(type, state, batchSize);
-      } catch(err) {
-        return reject(err);
-      }
-
-      canceledCount = batchResults.length;
-    } while (canceledCount === batchSize);
-
-    resolve();
-  });
-}
-
-function cancelJobsByStateBatch(type, state, batchSize) {
-  return new Promise((resolve, reject) => {
-    kue.Job.rangeByType(type, state, 0, batchSize, 'asc', async (err, jobs) => {
-      if (err) {
-        return reject(err);
-      }
-
-      Promise
-        .all(jobs.map(job => cancelIndividualJob(job)))
-        .then(resolve)
-        .catch(reject);
-    });
-  });
-}
-
-function cancelIndividualJob(job) {
-  return new Promise((resolve, reject) => {
-    job.remove(err => {
-      if (err) {
-        return reject(err);
-      }
-      resolve();
-    });
-  });
 }
 
 module.exports = Queue;
