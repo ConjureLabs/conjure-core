ea0f9853ee1fe9e82191dbf88381af1f8351a3a0
tmarshall <timothyjmarshall@gmail.com>
2018-04-15 14:22:45 -0700

    check for repo, create if does not exist

---
+++ modules/AWS/ECR/repo-exists.js
@@ -0,0 +1,29 @@
+const repoDoesNotExistExpr = /^The repository .* does not exist/
+
+// checks if the ECR repo exists
+module.exports = function repoExists(watchedRepoRecord) {
+  return new Promise((resolve, reject) => {
+    const getRepoName = require('./get-repo-name')
+    const repoName = getRepoName(watchedRepoRecord)
+
+    const AWS = require('../')
+    const ecr = new AWS.ECR()
+
+    // see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/ECR.html#getRepositoryPolicy-property
+    // using this method since no getRepository or defineResository method exists
+    ecr.getRepositoryPolicy({
+      repositoryName: repoName
+    }, err => {
+      if (err) {
+        // 'The repository with name \'conjure/development-watched-1\' does not exist in the registry with id \'657781215424\''
+        if (err.message && repoDoesNotExistExpr.test(err.message)) {
+          resolve(false)
+          return
+        }
+        reject(err)
+      }
+
+      resolve(true)
+    })
+  })
+}
