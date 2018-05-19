b19714f2809999aa87c9d2e384254a041a054d9a
tmarshall <timothyjmarshall@gmail.com>
2018-05-19 15:31:25 -0700

    give some context when github endpoint not found

--- classes/GitHub/API/index.js
+++ classes/GitHub/API/index.js
@@ -19,8 +19,9 @@ class API {
     ...opts
   }) {
     return new Promise((resolve, reject) => {
+      const pathPrepared = path.replace(leadingSlash, '')
       request({
-        url: `https://api.github.com/${path.replace(leadingSlash, '')}`,
+        url: `https://api.github.com/${pathPrepared}`,
         method,
         headers: this.headers,
         json: true,
@@ -36,8 +37,35 @@ class API {
             { message: 'Not Found',
               documentation_url: 'https://developer.github.com/v3' }
            */
-          return reject(new ConjureError(body.message || `Github returned code ${res.statusCode}`))
+          let message = body.message || `Github returned code ${res.statusCode}`
+          message = message === 'Not Found' ? `Not Found (/${pathPrepared})` : message
+          return reject(new ConjureError(message))
         }
+
+        // see https://developer.github.com/v3/#pagination
+        const pagination = {}
+        if (res.headers.Link) {
+          const expr = /^\s*<https:\/\/api\.github\.com\/([^>]+)>;\s+rel="([^"]+)"\s*$/
+          for (i = 0; i < 20; i++) { // 20 just to be safe
+            const match = expr.exec(res.headers.Link)
+
+            if (!match) {
+              break
+            }
+
+            /*
+              This will throw if `body` is not an object
+              If GitHub returns headers for `next` pagination, this will add `.next` to `.pagination` to `body`
+              `.pagination` is not normally enumerable
+             */
+            pagination[ match[2] ] = match[1]
+          }
+        }
+        Object.defineProperty(body, 'pagination', {
+          value: pagination,
+          enumerable: false,
+          configurable: false
+        })
 
         resolve(body)
       })
