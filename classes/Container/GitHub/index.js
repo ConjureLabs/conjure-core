c71e8ca59d2465a435a39d14ee28cc2dc7e5afd6
tmarshall <timothyjmarshall@gmail.com>
2018-05-20 11:53:45 -0700

    when stopped, show message

--- classes/Container/GitHub/index.js
+++ classes/Container/GitHub/index.js
@@ -24,6 +24,14 @@ class GitHubContainer extends Container {
     const issueComment = new IssueComment(this.payload)
     await issueComment.delete()
   }
+
+  async stop() {
+    await super.stop()
+
+    const issueComment = new IssueComment(this.payload)
+    const containerRequestUrl = `${config.app.web.url}/start/${orgName}/${repoName}/${number}`
+    await issueComment.upsert(`:ghost: [You can spin up this branch on Conjure](${containerRequestUrl})`)
+  }
 }
 
 GitHubContainer.prototype.getConfig = require('./get-config')
